c935aff8bfa6438c79145be7d73f13e1c0276d85
nicub97 <nbalica@myges.fr>
2025-07-13 08:43:48 +0100

    Delete backend directory

--- backend/resources/views/emails/code_depot.blade.php
+++
@@ -1,7 +0,0 @@
-Bonjour,
-
-Voici votre code de dépôt pour l'annonce "{{ $annonce->titre }}" :
-
-**{{ $code }}**
-
-Merci d'utiliser notre service.
--- backend/resources/views/emails/code_retrait.blade.php
+++
@@ -1,7 +0,0 @@
-Bonjour,
-
-Voici votre code de retrait pour l'annonce "{{ $annonce->titre }}" :
-
-**{{ $code }}**
-
-Merci d'utiliser notre service.
